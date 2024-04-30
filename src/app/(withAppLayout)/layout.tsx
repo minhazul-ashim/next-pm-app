86eda4027b4e7f27943d3e5dbddc600e40bd2c8b
Minhazul Ashim <ashim.ashim7470@gmail.com>
2024-04-30 15:08:28 +0600

    added layouts

---
+++ src/app/(withAppLayout)/layout.tsx
@@ -0,0 +1,13 @@
+import AppLayout from "@/layout/AppLayout";
+
+export default function WithAppLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <AppLayout>
+      {children}
+    </AppLayout>
+  );
+}
