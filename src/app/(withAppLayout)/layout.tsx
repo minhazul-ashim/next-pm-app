834da010107aeeab3e8fcd8ee54a916912f37b09
Minhazul Ashim <ashim.ashim7470@gmail.com>
2024-05-02 03:16:46 +0600

    will work on filtering now

--- src/app/(withAppLayout)/layout.tsx
+++ src/app/(withAppLayout)/layout.tsx
@@ -1,8 +1,6 @@
 "use client";
 
 import AppLayout from "@/layout/AppLayout";
-import { UserState, userStore } from "@/store/userStore";
-import { redirect } from "next/navigation";
 
 export default function WithAppLayout({
   children,
