0ab59452b421e25692108df937f2a83a327faa27
Minhazul Ashim <ashim.ashim7470@gmail.com>
2024-05-01 22:29:01 +0600

    added create task form

--- src/components/tasks/addTaskModal.tsx
+++ src/components/tasks/addTaskModal.tsx
@@ -1,5 +1,25 @@
 import React, { useState } from "react";
-import { Button, Modal } from "antd";
+import {
+  Button,
+  DatePicker,
+  DatePickerProps,
+  Form,
+  FormInstance,
+  Input,
+  Modal,
+  Select,
+} from "antd";
+
+interface Values {
+  title?: string;
+  description?: string;
+  modifier?: string;
+}
+
+interface CollectionCreateFormProps {
+  initialValues: Values;
+  onFormInstanceReady: (instance: FormInstance<Values>) => void;
+}
 
 const AddTaskModal = ({
   open,
@@ -8,6 +28,7 @@ const AddTaskModal = ({
   open: boolean;
   setOpen: React.Dispatch<React.SetStateAction<boolean>>;
 }) => {
+  const [form] = Form.useForm();
   const [loading, setLoading] = useState(false);
 
   const handleOk = () => {
@@ -16,6 +37,10 @@ const AddTaskModal = ({
       setLoading(false);
       setOpen(false);
     }, 3000);
+  };
+
+  const onOk = (value: DatePickerProps["value"]) => {
+    console.log("onOk: ", value);
   };
 
   const handleCancel = () => {
@@ -43,7 +68,47 @@ const AddTaskModal = ({
           </Button>,
         ]}
       >
-        
+        <Form
+          layout="vertical"
+          form={form}
+          name="form_in_modal"
+          //   initialValues={initialValues}
+        >
+          <Form.Item
+            name="title"
+            label="Title"
+            rules={[
+              {
+                required: true,
+                message: "Please enter task name",
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item name="description" label="Description">
+            <textarea rows={4} className="w-full rounded-md border" />
+          </Form.Item>
+          <Form.Item name="assignedTo" label="Assign To">
+            <Select>
+              <Select.Option value="sample">Sample</Select.Option>
+              <Select.Option value="sample">Sample</Select.Option>
+              <Select.Option value="sample">Sample</Select.Option>
+              <Select.Option value="sample">Sample</Select.Option>
+            </Select>
+          </Form.Item>
+          <Form.Item name="dueDate" label="Due Date">
+            <DatePicker
+              showTime
+              className="w-full"
+              onChange={(value, dateString) => {
+                console.log("Selected Time: ", value);
+                console.log("Formatted Selected Time: ", dateString);
+              }}
+              onOk={onOk}
+            />
+          </Form.Item>
+        </Form>
       </Modal>
     </>
   );
